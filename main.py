fb79779d7c8a39673cfd83c88efb583100621714
Thanh Dat <audacityvtd@gmail.com>
2025-07-06 12:44:23 +0700

    Added moves for the result of both DFS & A*, testing ok by main.py & main2.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ mask = (1 << 37) | (1 << 36)  # Xe nằm ngang tại (3,2)-(3,3)
 red_car = Vehicle(mask, H)
 initial_state = State(vehicle_list=[red_car])
 
-#path = dfs(initial_state, max_depth=20)
-#path = iterative_deepening_search(initial_state, max_depth=20)
-path = dfs_solver(initial_state, max_depth=50)
+path, moves = dfs_solver(initial_state, max_depth=50)
 with open("output.txt", "w", encoding="utf-8") as f:
     f.write("=== TRẠNG THÁI BAN ĐẦU ===\n")
     f.write(str(initial_state) + "\n")
@@ -16,10 +14,15 @@ with open("output.txt", "w", encoding="utf-8") as f:
     f.write(f"Vertical mask: {bin(initial_state.ver_mask)}\n")
     f.write(f"Is goal: {initial_state.is_goal()}\n\n")
 
-    if path:
-        f.write(f" Tìm thấy lời giải với {len(path)-1} bước:\n\n")
+    if path and moves:
+        f.write(f"(Kiểm tra path) Tìm thấy lời giải với {len(path)-1} bước:\n")
+        f.write(f"(Kiểm tra moves) Tìm thấy lời giải với {len(moves)} bước:\n\n")
         for idx, s in enumerate(path):
-            f.write(f"--- Bước {idx} ---\n")
+            f.write(f"--- State a{idx} ---\n")
+            f.write(str(s) + "\n")
+            
+        for idx, s in enumerate(moves):
+            f.write(f"--- Bước b{idx} ---\n")
             f.write(str(s) + "\n")
     else:
         f.write(" Không tìm thấy lời giải.\n")
